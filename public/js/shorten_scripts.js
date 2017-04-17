4709c905c7ef9eff9033afd2a8f3c11f690fe6a4
Jon Janelle <jjanelle@g.harvard.edu>
2017-04-17 14:54:13 -0400

    mobile formatting update

--- public/js/shorten_scripts.js
+++ public/js/shorten_scripts.js
@@ -7,9 +7,3 @@ function copyToClipboard() {
 
     $('#copy-msg').html("Copied to clipboard!");
 }
-
-$(document).ready(function() {
-  $(window).load(function() { $("html, body").animate({
-    scrollTop: $(document).height() }, 0);
-  });
-)};
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,10 +44,7 @@
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
